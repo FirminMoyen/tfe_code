ddf17bc63f655842e7014b81479b6723be6866d0
FirminMoyen <firminmoyen@gmail.com>
2024-04-15 11:45:37 +0200

    update sidebar

--- salamander_id/src/components/sidebar.jsx
+++ salamander_id/src/components/sidebar.jsx
@@ -1,34 +1,31 @@
-import React, {useState} from 'react';
+import React, { useState } from "react";
 import SideBarTab from "./sidebartab";
 
-
 function SideBar() {
+  const tabsdata = [
+    { id: 1, name: "Home" },
+    { id: 2, name: "Library" },
+    { id: 3, name: "Import" },
+    { id: 4, name: "review" },
+    { id: 5, name: "Backup" },
+    { id: 6, name: "Mail" },
+    { id: 7, name: "Bénévols" },
+  ];
 
-    const tabsdata = [
-        { id: 1, name: "Home" },
-        { id: 2, name: "Library" },
-        { id: 3, name: "Import" },
-        { id: 4, name: "review" },
-        { id: 5, name: "Backup" },
-        { id: 6, name: "Mail" },
-        { id: 7, name: "Bénévols" },
-    ];
+  const [selectedTab, setSelectedTab] = useState(tabsdata[0].name);
 
-    const [selectedTab, setSelectedTab] = useState(tabsdata[0].name);
+  function handleTabClick(event) {
+    const tabName = event.target.textContent;
+    console.log(event.target);
+  }
 
-    function handleTabClick(event) {
-        const tabName = event.target.textContent;
-        console.log(event.target)
-    }
-
-    return <div className="sidebar">
-        {tabsdata.map( tab => 
-                <SideBarTab onclick={handleTabClick}
-                    key={tab.id} 
-                    name={tab.name} 
-                />
-        )}
-    </div>;
+  return (
+    <div className="sidebar">
+      {tabsdata.map((tab) => (
+        <SideBarTab onclick={handleTabClick} key={tab.id} name={tab.name} />
+      ))}
+    </div>
+  );
 }
 
 export default SideBar;
